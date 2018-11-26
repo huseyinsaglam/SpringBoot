50bc52b830e9ea5ae76366f4b657c3396cce590f
rails -v <hsaglam001@gmail.com>
2018-11-26 21:39:58 +0300

    JaxbXML

---
+++ SpringBoot/JaxbXML/src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,65 @@
+package com.example.demo.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+//@XmlRootElement(name = "person")
+//@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
+public class Person {
+
+	
+	private String id;
+	private String name;
+	private String surname;
+	private String birthYear;
+
+	public Person() {
+		// default constructor zorunlu.
+		// com.sun.xml.internal.bind.v2.runtime.IllegalAnnotationsException:
+		super();
+	}
+
+	public Person(String id, String name, String surname, String birthYear) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.birthYear = birthYear;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getBirthYear() {
+		return birthYear;
+	}
+
+	public void setBirthYear(String birthYear) {
+		this.birthYear = birthYear;
+	}
+
+}
