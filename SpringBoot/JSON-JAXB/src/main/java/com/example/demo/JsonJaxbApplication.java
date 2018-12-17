28ffc2da1bea4ce8ffccb9e9a27466eb53961e1b
rails -v <hsaglam001@gmail.com>
2018-12-17 14:27:35 +0300

    Spring Boot Çalışmam
    Spring Boot Çalışmam

---
+++ SpringBoot/JSON-JAXB/src/main/java/com/example/demo/JsonJaxbApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonJaxbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonJaxbApplication.class, args);
+	}
+}
